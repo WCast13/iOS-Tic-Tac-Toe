b775efaaeaabdc6d7555ef4a22ef140b047e9fa3
William Castellano <wcastellano13@gmail.com>
2017-01-18 01:08:48 -0500

    The correct pieces come up, players alternate, and only empty squares are clickable

--- ticTacToe/ticTacToe/ViewController.swift
+++ ticTacToe/ticTacToe/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+//  1 is O's, 2 is X's
+  var activePlayer = arc4random_uniform((2) + 1)
+  var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0] // 0- empty, 1 O's, 2 X's
  
   @IBAction func buttonPressed(_ sender: AnyObject) {
-    print(sender.tag)
-  
+    
+  var activePosition = sender.tag - 1
+    
+    if gameState[activePosition] == 0 {
+      gameState[activePosition] = Int(activePlayer)
+      
+      if activePlayer == 1 {
+        sender.setImage(UIImage(named: "nought.png"), for: [])
+        activePlayer = 2
+        
+      } else {
+        sender.setImage(UIImage(named: "cross.png"), for: [])
+        activePlayer = 1
+      }
+    }
   }
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
